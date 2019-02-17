426bf09fdba320f9aedb3a4e70c69209b4ef7e17
TeresaGarcia <teresagar181@gmail.com>
2019-02-17 23:16:54 +0100

    Signed-off-by: TeresaGarcia <teresagar181@gmail.com>

---
+++ src/main/java/es/salesianos/model/Account.java
@@ -0,0 +1,24 @@
+package es.salesianos.model;
+
+public class Account {
+
+	private Integer id;
+	private Integer balance;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Integer balance) {
+		this.balance = balance;
+	}
+
+}
